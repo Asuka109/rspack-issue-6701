0b0eee4a048cc36f7024f03ff14983edd8face57
Asuka109 <asuka109@foxmail.com>
2024-06-02 00:47:05 +0800

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+// It works fine ⬇️.
+import 'data:text/javascript,import "core-js";';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
